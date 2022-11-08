35fc8b472145b3f5f40efdf737c66c5a7cc3ad1a
19Lida <lida19melnyk@gmail.com>
2022-11-08 23:34:41 +0200

    додає таск 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkUp = createImgCard(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkUp);
+function createImgCard(images) {
+    return images.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>`;
+    })
+    .join('');
+}
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+function onGalleryContainerClick(event){
+    const imageItem = document.querySelector(".gallery__image");
+    event.preventDefault();
+    console.log(imageItem.dataset.source);
+console.log(event.target);
+
+     var lightbox = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionDelay: 250,
+        animationSpeed: 250,
+    });
+
+};
+
 
 console.log(galleryItems);
